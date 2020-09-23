4c9befdc26f9528eb03274d44a96e3791df909f7
Dmytro Grendach <dmytro.grendach@beamtrail.com>
2020-09-23 22:44:46 +0400

    add docker build command

---
+++ build_using_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker run --rm \
+  -e PACKER_LOG=1 \
+  -e PACKER_LOG_PATH="packer-docker.log" \
+  -it \
+  --privileged \
+  --cap-add=ALL -v /lib/modules:/lib/modules \
+  -v `pwd`:/opt/ \
+  -w /opt/ goffinet/packer-qemu build focal.json
